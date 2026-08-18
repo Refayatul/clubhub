9619ccacc1635a733674a3f16ceae8c1a777afe0
Md. Refayatul Islam <13129292+Refayatul@users.noreply.github.com>
2026-08-19 02:30:29 +0600

    Remove start.sh (DOM Cloud no longer used)

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Wrapper script for DOM Cloud / Phusion Passenger
-# Passenger executes this via bash, so $PORT env var is available
-exec java -Xmx350m -jar "$(dirname "$0")/target/Clubhub4-0.0.1-SNAPSHOT.jar"
